58dab912d2ae3982d54b8fbf27f3042bc2ba5ffa
Trong Nguyen (Ext Developer) <trong.nguyen@finos.asia>
2022-08-04 21:06:00 +0700

    load js admin page success

---
+++ wp-content/plugins/are-you-paying-attention/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+	Plugin Name: Are You Paying Attention
+	Description: Are You Paying Attention Plugin
+	Version: 1.0
+	Author: Danh996
+	Author URI: https://danh996.com
+*/ 
+
+if(! defined('ABSPATH')) exit;
+
+class AreYouPayingAttension{
+    function __construct(){
+    	add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+    }
+
+    function adminAssets(){
+    	wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks', 'wp-element'));
+    }
+}
+
+$areaYouPayingAttention = new AreYouPayingAttension();
--- wp-content/plugins/are-you-paying-attention/test.js
+++ wp-content/plugins/are-you-paying-attention/test.js
@@ -1,1 +1,11 @@
-test.js
+wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
+	title: "Are You Paying Attenttion?",
+	icon: "smiley",
+	category: "common",
+	edit: function(){
+		return wp.element.createElement("h3", null, "Hello, this is from the admin")
+	},
+	save: function(){
+		return wp.element.createElement("h1", null, "Hello, this is the frontend")
+	}
+});
